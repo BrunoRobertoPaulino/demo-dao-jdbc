bb473ca8cc8595423dc419505d245d5866734771
Bruno Roberto <brunoroberto.paulino@gmail.com>
2024-07-27 08:08:47 -0300

    findById implementation

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,73 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			
+			st = conn.prepareStatement(
+					"SELECT Id, Name FROM department WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department department = new Department(rs.getInt("Id"), rs.getString("Name"));
+				return department;
+			}
+			return null;
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {			
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);	
+		}
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
